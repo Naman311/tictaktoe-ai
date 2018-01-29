7fbadfb544b66be1eced024eb299e8022924b3b2
Naman Gupta <namgupta96@gmail.com>
2018-01-29 11:09:25 +0530

    reduced for loops

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include<iostream>
 
 using namespace std;
-int arr[3][3]={0};
-
+int arr[9]={0};
 int check_plus()
 {
-    for(int i=0;i<3;i++)
+    for(int i=0,j=0;i<3;i++,j=j+3)
     {
-        if(arr[i][0]==arr[i][1] && arr[i][1]==arr[i][2])
+        if((arr[0+i]+arr[3+i]+arr[6+i]==3) || arr[0+j]+arr[1+j]+arr[2+j]==3)
         {
-            return arr[i][0];
+            return 1;
         }
-        else if(arr[0][i]==arr[1][i] &&arr[1][i]==arr[2][i])
+        else if(arr[0+i]+arr[3+i]+arr[6+i]==0 || arr[0+j]+arr[1+j]+arr[2+j]==0)
         {
-            return arr[0][i];
+            return 0;
         }
     }
     return -1;
 }
-bool check_x(int ch)
+bool check_x()
 {
-    if((arr[0][0]==ch && arr[2][2] ==ch) || (arr[0][2]==ch && arr[2][0]==ch))
+    if((arr[0]==arr[4] && arr[4]==arr[8]) || (arr[2]==arr[4]&& arr[4]==arr[6]))
         return true;
     else
         return false;
@@ -31,47 +30,39 @@ void userinput(int i)
     cin>>x;
     switch(x)
     {
-        case 1:arr[0][0]=i;break;
-        case 2:arr[0][1]=i;break;
-        case 3:arr[0][2]=i;break;
-        case 4:arr[1][0]=i;break;
-        case 5:arr[1][1]=i;break;
-        case 6:arr[1][2]=i;break;
-        case 7:arr[2][0]=i;break;
-        case 8:arr[2][1]=i;break;
-        case 9:arr[2][2]=i;break;
+        case 1:arr[0]=i;break;
+        case 2:arr[1]=i;break;
+        case 3:arr[2]=i;break;
+        case 4:arr[3]=i;break;
+        case 5:arr[4]=i;break;
+        case 6:arr[5]=i;break;
+        case 7:arr[6]=i;break;
+        case 8:arr[7]=i;break;
+        case 9:arr[8]=i;break;
     }
 }
 void showmap()
 {
-    for(int i=0; i<3;i++)
+    for(int i=0; i<9;i++)
     {
-        for(int j=0;j<3;j++)
-        {
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
+        (i==2 || i==5)?cout<<arr[i]<<endl:cout<<arr[i]<<" ";
     }
 }
 void checkwin()
 {
-    for(int i=0; i<3;i++)
+    for(int i=0; i<9;i++)
     {
-        for(int j=0;j<3;j++)
+        if(check_x())
         {
-            int ch=arr[1][1];
-            if(check_x(ch))
-            {
-                cout<<"player "<<ch<<" win with x";
-                return;
-            }
-            ch=check_plus();
-            if(ch==-1)
-                cout<<"DRAW";
-            else
-                cout<<"player "<<ch<<" wins";
-            return ;
+            cout<<"player "<<arr[4]<<" win with x";
+            return;
         }
+        int ch=check_plus();
+        if(ch==-1)
+            cout<<"DRAW";
+        else
+            cout<<"player "<<ch<<" wins";
+        return ;
     }
 }
 int main()
@@ -81,6 +72,7 @@ int main()
     for(int i=1;i<10;i++)
     {
         userinput(i%2);
+
     }
     showmap();
     checkwin();
